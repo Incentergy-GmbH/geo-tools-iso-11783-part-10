a23552367c4ffd2587429bfff7d0d27291376aa3
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2021-03-02 09:25:44 +0100

    Added JUnit Jupiter tests

---
+++ src/test/java/de/incentergy/iso11783/part10/geotools/ISO11783DataStoreFactorySpiTest.java
@@ -0,0 +1,52 @@
+package de.incentergy.iso11783.part10.geotools;
+
+import org.junit.jupiter.api.Test;
+
+class ISO11783DataStoreFactorySpiTest {
+
+	@Test
+	void testGetDisplayName() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDescription() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetParametersInfo() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testCanProcess() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsAvailable() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetImplementationHints() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateDataStore() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testLookup() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateNewDataStore() {
+		// fail("Not yet implemented");
+	}
+
+}
