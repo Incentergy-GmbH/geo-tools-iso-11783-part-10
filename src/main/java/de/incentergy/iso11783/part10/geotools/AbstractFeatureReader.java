e0a6c3aba8132c130134379149449acecf4dc826
Alexander Parshin <parshin.alexander@gmail.com>
2021-03-02 12:13:38 +0300

    Partfield Reader implementation

--- src/main/java/de/incentergy/iso11783/part10/geotools/AbstractFeatureReader.java
+++ src/main/java/de/incentergy/iso11783/part10/geotools/AbstractFeatureReader.java
@@ -1,7 +1,10 @@
 package de.incentergy.iso11783.part10.geotools;
 
 import org.geotools.data.FeatureReader;
+import org.geotools.data.store.ContentState;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 
-public abstract class AbstractFeatureReader implements FeatureReader {
-
+public abstract class AbstractFeatureReader implements FeatureReader<SimpleFeatureType, SimpleFeature>  {
+    protected ContentState state;
 }
--- src/main/java/de/incentergy/iso11783/part10/geotools/PartfieldFeatureReader.java
+++ src/main/java/de/incentergy/iso11783/part10/geotools/PartfieldFeatureReader.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.util.NoSuchElementException;
 
 import org.geotools.feature.NameImpl;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.locationtech.jts.geom.MultiPolygon;
 import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.Name;
 
@@ -13,9 +17,32 @@ public class PartfieldFeatureReader extends AbstractFeatureReader{
 	public static final String TYPE_NAME_STRING = "Partfield";
 	public static final Name TYPE_NAME = new NameImpl(PartfieldFeatureReader.class.getPackageName(), TYPE_NAME_STRING);
 
+
+    protected SimpleFeatureType buildFeatureType() throws IOException {
+
+        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+        builder.setName(TYPE_NAME_STRING);
+
+        builder.setCRS(DefaultGeographicCRS.WGS84); // <- Coordinate reference system
+        builder.add("polygonNonTreatmentZoneOnly", MultiPolygon.class);
+        builder.add("partfieldId", String.class);
+        builder.add("partfieldCode", String.class);
+        builder.add("partfieldDesignator", String.class);
+        builder.add("partfieldArea", Long.class);
+        builder.add("customerIdRef", String.class);
+        builder.add("partfieldId", String.class);
+        builder.add("farmIdRef", String.class);
+        builder.add("cropTypeIdRef", String.class);
+        builder.add("cropVarietyIdRef", String.class);
+        builder.add("fieldIdRef", String.class);
+
+        final SimpleFeatureType SCHEMA = builder.buildFeatureType();
+        return SCHEMA;
+    }
+
 	@Override
 	public FeatureType getFeatureType() {
-		return null;
+		return state.getFeatureType();
 	}
 
 	@Override
